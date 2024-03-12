ed51e62199c724bd23c283a576cec5e3ae5a9626
Gabriel <gab.serrano@duocuc.cl>
2024-03-12 16:13:37 -0300

    Renderiza hero

--- frontend/moviesrev-v1/src/api/axiosConfig.js
+++ frontend/moviesrev-v1/src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
 
-  baseURL: "https://4ceb-186-79-99-43.ngrok-free.app",
+  baseURL: "https://d794-186-79-99-43.ngrok-free.app",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
