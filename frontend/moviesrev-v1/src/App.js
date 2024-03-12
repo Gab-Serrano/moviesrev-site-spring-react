e2da8e3f716a2055023b7412baeca24292f932f0
Gabriel <gab.serrano@duocuc.cl>
2024-03-12 12:54:05 -0300

    Crea home y hero con carousel

--- frontend/moviesrev-v1/src/App.js
+++ frontend/moviesrev-v1/src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import api from "./api/axiosConfig";
 import { useEffect, useState } from "react";
 import Layout from "./components/Layout";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movie/getAllMovies");
-      console.log(response.data);
       setMovies(response.data);
     } catch (error) {
       console.error(error);
@@ -21,13 +21,15 @@ function App() {
     getMovies();
   }, []);
 
-  return <div className="App">
-    <Routes>
-      <Route path="/" element={Layout}>
-        
-      </Route>
-    </Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
